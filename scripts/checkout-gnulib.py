79400a981b667dfd87dab1579f81c4dd642368e8
Vincent Schweiger <vinsch32@icloud.com>
2026-03-01 22:41:31 +0100

    feat: get a simple CMake Yak compiled

---
+++ scripts/checkout-gnulib.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Copyright 2014-2020 Managarm Contributors
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+import argparse
+import pathlib
+import subprocess
+import sys
+
+def main():
+	parser = argparse.ArgumentParser(description="Helper for checking out gnulib")
+	parser.add_argument("repo", type=pathlib.Path, help="path to the repo")
+	parser.add_argument("path", type=pathlib.Path, default="gnulib", help="where to check out gnulib to", nargs='?')
+
+	args = parser.parse_args()
+
+	if not (args.path / '.git').exists():
+		result = subprocess.run(["git", "clone", args.repo, args.path])
+		sys.exit(result.returncode)
+	sys.exit(0)
+
+if __name__ == "__main__":
+    main()
